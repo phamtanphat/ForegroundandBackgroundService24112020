e3b85306fa7df5938eea02cb7789e87312f3a6f4
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-02-23 19:07:22 +0700

    xử lý task  trong service

--- app/src/main/java/com/example/foregroundandbackgroundservice24112020/MainActivity.java
+++ app/src/main/java/com/example/foregroundandbackgroundservice24112020/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.foregroundandbackgroundservice24112020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
         mBtnBackground.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(MainActivity.this,MyBackgroundService.class);
+                intent.putExtra("chuoi","hello");
+                startService(intent);
             }
         });
     }
--- app/src/main/java/com/example/foregroundandbackgroundservice24112020/MyBackgroundService.java
+++ app/src/main/java/com/example/foregroundandbackgroundservice24112020/MyBackgroundService.java
@@ -2,14 +2,18 @@ package com.example.foregroundandbackgroundservice24112020;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 public class MyBackgroundService extends Service {
 
     // Chỉ dùng khi các dùng bound service
+    Handler handler;
+    int i = 0;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -19,12 +23,28 @@ public class MyBackgroundService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("BBB","onCreate Service");
+        Toast.makeText(this, "onCreate Service", Toast.LENGTH_SHORT).show();
+        handler = new Handler();
+//        Log.d("BBB","onCreate Service");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("BBB","onStartCommand");
-        return super.onStartCommand(intent, flags, startId);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (i <= 100){
+                    Toast.makeText(MyBackgroundService.this, "Giá trị của i : " + i, Toast.LENGTH_SHORT).show();
+                    i++;
+                    handler.postDelayed(this , 2000);
+                }else{
+                    Toast.makeText(MyBackgroundService.this, "Kết thúc", Toast.LENGTH_SHORT).show();
+                }
+            }
+        },2000);
+
+
+        return START_NOT_STICKY;
     }
+
 }
